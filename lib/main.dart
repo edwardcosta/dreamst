108d3e52798325e55c5f1827f0b2b2254eac0ce7
Eduardo Calandrini <eduardocalandrini@hotmail.com>
2024-01-17 23:08:52 +0000

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:dreamst/constants/projectColors.dart';
+import 'package:dreamst/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/home/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
